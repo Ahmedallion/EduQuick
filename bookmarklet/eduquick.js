a9f7236ec469d947b0df7371d05bd613a6ba58e6
Tamim Ahmed <170207679+Ahmedallion@users.noreply.github.com>
2025-11-16 13:30:34 +0000

    Clean up

--- bookmarklet/eduquick.js
+++
@@ -1,6 +0,0 @@
-javascript: (function () {
-    var s = document.createElement("script");
-    s.src =
-        "https://raw.githubusercontent.com/Ahmedallion/EduQuick/main/dist/eduquick.min.js";
-    document.body.appendChild(s);
-})();
--- bookmarklet/test.js
+++
@@ -1,5 +0,0 @@
-javascript: (function () {
-    var s = document.createElement("script");
-    s.src = "http://localhost:3000/eduquick.min.js";
-    document.body.appendChild(s);
-})();
